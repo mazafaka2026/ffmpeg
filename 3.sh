b09abd758f68bd3146cfb429829dca62942fb079
mazafaka2026 <mazafaka2026@bk.ru>
2019-03-16 13:35:25 +0600

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,1 +1,1 @@
-ffmpeg -i cropped.mp4 -i plate.png -filter_complex "overlay=0:446:enable='between(t,1,9)'" -c:a copy plated.mp4
+ffmpeg -i cropped.mp4 -i plate.png -filter_complex "overlay=0:446:enable='between(t\,1\,9)'" -c:a copy plated.mp4
